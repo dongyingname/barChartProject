e4e3e4b0f713053a3dc119517729a6c51bb6081a
Ying Dong <dongyingname@yahoo.com>
2018-09-23 23:42:08 +0000

    I haven't really understood the functional requirements. what is the element input?

---
+++ drawBarChart.js
@@ -0,0 +1,10 @@
+function drawBarChart(data, options, element){
+/* data could be an array of numbers
+  options could be an array of styles {color, font}
+  element is either  x lable y lable etc
+
+*/
+
+
+
+}
